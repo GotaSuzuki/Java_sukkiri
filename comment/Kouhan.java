627d8b712989ffeff5df97087d41ac7672c402e3
GotaSuzuki <go.baseball.0408@icloud.com>
2023-02-16 19:37:52 +0900

    クラスの書き方

---
+++ comment/Kouhan.java
@@ -0,0 +1,11 @@
+package comment;
+public class Kouhan {
+  public static void callDeae(){
+    System.out.println("エェイ、小癪な");
+  }
+  public static void showMondokoro() throws Exception{
+    System.out.println("飛車さん、角さん");
+    System.out.println("この紋所が目に入らぬか");
+    Zenhan.doTogame();
+  }
+}
